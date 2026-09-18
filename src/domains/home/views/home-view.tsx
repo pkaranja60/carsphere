45b0f2945f10fdc51b0dd530bae0d7d63e5e4f9d
Peter karanja <peterkaranja60@gmail.com>
2026-09-19 02:55:34 +0300

    style(home): adjust background overlay opacity

--- src/domains/home/views/home-view.tsx
+++ src/domains/home/views/home-view.tsx
@@ -21,7 +21,7 @@ export function HomeView() {
   return (
     <div className="relative flex w-full flex-col">
       <div
-        className="pointer-events-none absolute inset-0 z-0 opacity-[0.12] mix-blend-multiply dark:opacity-[0.15] dark:mix-blend-screen dark:invert"
+        className="pointer-events-none absolute inset-0 z-0 opacity-[0.09] mix-blend-multiply dark:opacity-[0.15] dark:mix-blend-screen dark:invert"
         style={{
           backgroundImage: "url(/images/splatter-bg.jpg)",
           backgroundRepeat: "repeat",
